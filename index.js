e1f0aa0513c092fa69ac2a220802bfd9af5a278f
soyelchecho <sergiorojas12ocm@gmail.com>
2023-10-18 18:36:20 -0500

    Add start server and destroy server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Websocket = require('ws');
-const { spawn } = require('child_process');
+const exec = require('child_process').exec;
 const AWS = require('aws-sdk');
 const util = require('util');
 const path = require('path');
 
 const wss = new Websocket.Server({ port : 4000});
 const metadata = new AWS.MetadataService();
+
+process.stdin.resume(); // so the program will not close instantly
+
 
 const MIN_PORT_AVAILABILITY = 7777;
 const MAX_PORT_AVAILABILITY = 8000;
@@ -31,39 +34,115 @@ function getAvailablePort(){
 
 global.hostIpAddress = null;
 
+playersConnected = 0;
+listOfServers = [];
+listOfProcess = [];
+
+const RANGEELO = [
+    { min: 0, max: 49 },      { min: 50, max: 99 },
+    { min: 100, max: 149 },   { min: 150, max: 199 },
+    { min: 200, max: 249 },   { min: 250, max: 299 },
+    { min: 300, max: 349 },   { min: 350, max: 399 },
+    { min: 400, max: 449 },   { min: 450, max: 499 },
+    { min: 500, max: 549 },   { min: 550, max: 599 },
+    { min: 600, max: 649 },   { min: 650, max: 699 },
+    { min: 700, max: 749 },   { min: 750, max: 799 },
+    { min: 800, max: 849 },   { min: 850, max: 899 },
+    { min: 900, max: 949 },   { min: 950, max: 999 },
+    { min: 1000, max: 1049 }
+  ]
+
+
 function continueAfterGetIp(){
     let DedicatedServer = {
         host: global.hostIpAddress,
+        recommendedElo: { min: 60, max: 150 },
         port: getAvailablePort(),
         status: 'running',
         playerCount: 0
     }
     console.log(DedicatedServer);
     wss.on('connection', (ws) => {
-        if(DedicatedServer.playerCount >= 2){
-            DedicatedServer.status = 'full';
-        }
-        else
-        {
-            DedicatedServer.playerCount++;
-        }
-    
-        if(DedicatedServer.status == 'full'){
-            DedicatedServer.port = Math.floor(Math.random() * (MAX_PORT_AVAILABILITY - MIN_PORT_AVAILABILITY + 1)) + MIN_PORT_AVAILABILITY;
-            DedicatedServer.status = 'running';
-            DedicatedServer.playerCount = 0;
-            StartDedicatedServer();
-        }
-        ws.send(JSON.stringify(DedicatedServer));
+        playersConnected++;
+
+        ws.on('message', function message(data){
+            const ParsedData = JSON.parse(data);
+            if(ParsedData.type == 'findingmatch'){
+                const {PlayerID, elo} = ParsedData.data;
+                serverForPlayer = findOrCreateServer(elo);
+                // Envía la información de serverForPlayer de vuelta al cliente
+                ws.send(JSON.stringify({ type: 'servermatch', data: serverForPlayer }));
+            }
+        })
     });
-    StartDedicatedServer();
 }
 
-function StartDedicatedServer(){
+// Función para encontrar o crear un servidor
+function findOrCreateServer(elo) {
+    for (let i = 0; i < listOfServers.length; i++) {
+      const server = listOfServers[i];
+      if (elo >= server.recommendedElo.min && elo <= server.recommendedElo.max) {
+        // Se encontró un servidor adecuado
+        return server;
+      }
+    }
+  
+    // Si no se encontró un servidor adecuado, llama a la función para crear un nuevo servidor
+    const newServer = createServer(elo); // Debes definir la función createServer
+    listOfServers.push(newServer);
+    return newServer;
+}
+
+
+function findEloRange(elo) {
+    for (let i = 0; i < RANGEELO.length; i++) {
+        const range = RANGEELO[i];
+        if (elo >= range.min && elo <= range.max) {
+        return range;
+        }
+    }
+    return null; // Devuelve null si no se encuentra ningún rango
+}
+
+function createServer(elo){
+    let NewDedicatedServer = {
+        host: global.hostIpAddress,
+        recommendedElo: findEloRange(elo),
+        port: getAvailablePort(),
+        status: 'running',
+        playerCount: 0
+    };
+
+    StartNewDedicatedServer(NewDedicatedServer);
+
+    return NewDedicatedServer;
+}
+
+function StartNewDedicatedServer(ServerToStart){
     // Obtén el path absoluto
     const absolutePathServerExec = path.resolve(__dirname, SERVER_EXECUTE_RELATIVE_PATH);
+    console.log('Path absoluto del server: ', absolutePathServerExec);
+    portToUse = ServerToStart.port;
 
-    console.log('Path absoluto: ', absolutePathServerExec);
+    const serverStartCommand = absolutePathServerExec + ' -log -port ${portToUse}';
+    backgroundServerProcess = exec(serverStartCommand, { detached: true, stdio: ['ignore', 'pipe', 'pipe'] });
+    // Desconectamos el proceso principal del proceso en segundo plano
+    backgroundServerProcess.unref();
+    // Manejo de la salida estándar y los errores
+    backgroundServerProcess.stdout.on('data', (data) => {
+        console.log(`Salida estándar: ${data}`);
+    });
+
+    backgroundServerProcess.stderr.on('data', (data) => {
+        console.error(`Error estándar: ${data}`);
+    });
+
+    backgroundServerProcess.on('close', (code) => {
+        console.log(`Proceso en segundo plano se cerró con código de salida ${code}`);
+    });
+
+    console.log('Proceso en segundo plano iniciado.');
+    listOfProcess.push(backgroundServerProcess);
 }
 
 
@@ -90,3 +169,32 @@ function initApp() {
 
 // Llamar a la función de inicialización cuando se inicia la aplicación
 initApp();
+
+
+function stopAllBackgroundProcesses() {
+    listOfProcess.forEach((backgroundProcess) => {
+      process.kill(-backgroundProcess.pid); // Mata el proceso en segundo plano y sus hijos
+      console.log('Proceso en segundo plano detenido.');
+    });
+    listOfProcess.length = 0; // Vaciar la lista
+}
+
+function exitHandler(options, exitCode) {
+    stopAllBackgroundProcesses();
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
+// do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+// catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+// catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
